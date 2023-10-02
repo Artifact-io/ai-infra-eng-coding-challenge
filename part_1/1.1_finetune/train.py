929d32db6789e9068c603a2fe40c557d20c90bd1
Caleb Bartholomew <caleb@artifact.io>
2023-10-02 11:06:46 -0600

    Changed model to roneneldan/TinyStories-28M

--- part_1/1.1_finetune/train.py
+++ part_1/1.1_finetune/train.py
@@ -1,12 +1,21 @@
-from transformers import AutoTokenizer, AutoModelForCasualLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
+from datasets import load_dataset
 
-
-MODEL_NAME = "tiiuae/falcon-rw-1b"
+MODEL_NAME = "roneneldan/TinyStories-28M"
 DATASET_NAME = "b-mc2/sql-create-context"
 
 def main():
+	# Get the dataset and format the data for training
+	dataset = load_dataset(DATASET_NAME, split="train")
+	dataset = dataset.map(
+		lambda examples: {"text": f"{examples['context']}\n{examples['question']}\n\n{examples['answer']}\n---"},
+		batched=False,
+		num_proc=4,
+		remove_columns=dataset["train"].column_names,
+	)
+
 	# Train the model
 	pass
 
